d466822f66a0cac029b21f25246b9da3c0e35b6a
Nataliia Bokhonska <nataliia@dealpotential.com>
2025-07-17 15:27:00 +0300

    feature 6UbwOUr6 add methods to notebook class

--- src/models/note_book.py
+++ src/models/note_book.py
@@ -1,7 +1,27 @@
 from collections import UserList
 
+from models.note import Note
+
 
 class NoteBook(UserList):
     """Represents a storage for Notes"""
 
-    pass
+    def add_note(self, note: Note) -> None:
+        """Add note into the NoteBook"""
+
+        self.data.append(note)
+
+    def find_by_note_id(self, id: int) -> Note | None:
+        """Find note by id in the NoteBook"""
+
+        for note in self.data:
+            if note.id.value == id:
+                return note
+
+    def remove_by_note_id(self, id: int) -> None:
+        """Remove note by id from the book"""
+
+        for note in self.data:
+            if note.id.value == id:
+                self.data.remove(note)
+                return
